b9c6873fc2512f35bb778d0bdb0565c9a9d83422
Graham Ambrose <graham.ambrose@bjss.com>
2020-12-01 10:07:12 +0000

    Day 1, part 2

--- src/AdventOfCode2020/Day1.cs
+++ src/AdventOfCode2020/Day1.cs
@@ -9,17 +9,9 @@ namespace AdventOfCode2020
     public class Day1
     {
         [Fact]
-        public void Example()
+        public void Part_1_example()
         {
-            var numbers = new[]
-            {
-                1721,
-                979,
-                366,
-                299,
-                675,
-                1456
-            };
+            var numbers = Example();
 
             var (a, b) = SumOfTwoNumbers(numbers.AsSpan(), 2020);
 
@@ -31,11 +23,9 @@ namespace AdventOfCode2020
         }
 
         [Fact]
-        public async Task Part1()
+        public async Task Part_1()
         {
-            var lines = await File.ReadAllLinesAsync("input.txt");
-
-            var numbers = lines.Select(int.Parse).ToArray();
+            var numbers = await Input();
 
             var (a, b) = SumOfTwoNumbers(numbers.AsSpan(), 2020);
 
@@ -43,6 +33,47 @@ namespace AdventOfCode2020
             var answer = a * b;
 
             Assert.Equal(545379, answer);
+        }
+
+        [Fact]
+        public void Part_2_example()
+        {
+            var numbers = Example();
+
+            var (a, b, c) = SumOfThreeNumbers(numbers.AsSpan(), 2020);
+
+            Assert.Equal(2020, a + b + c);
+
+            Assert.Equal(241861950, a * b * c);
+        }
+
+        [Fact]
+        public async Task Part_2()
+        {
+            var numbers = await Input();
+
+            var (a, b, c) = SumOfThreeNumbers(numbers.AsSpan(), 2020);
+
+            Assert.Equal(2020, a + b + c);
+
+            Assert.Equal(257778836, a * b * c);
+        }
+
+        private static int[] Example() => new[]
+        {
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456
+        };
+
+        private static async Task<int[]> Input()
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+
+            return lines.Select(int.Parse).ToArray();
         }
 
         private static (int, int) SumOfTwoNumbers(Span<int> numbers, int total)
@@ -72,6 +103,29 @@ namespace AdventOfCode2020
             }
 
             return (min, max);
+        }
+
+        private static (int, int, int) SumOfThreeNumbers(Span<int> numbers, int total)
+        {
+            numbers.Sort();
+
+            while (numbers.Length > 3)
+            {
+                if (numbers[0] + numbers[1] + numbers[^1] > total)
+                {
+                    numbers = numbers.Slice(0, numbers.Length - 1);
+                }
+                else if (!numbers.Contains(total - numbers[^1] - numbers[0]))
+                {
+                    numbers = numbers.Slice(1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return (numbers[0], numbers[1], numbers[^1]);
         }
     }
 }
