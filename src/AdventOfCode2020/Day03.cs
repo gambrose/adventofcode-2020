1f996c25aff6806749947a13970bad92372ba7b6
Graham Ambrose <graham.ambrose@gmail.com>
2020-12-03 21:02:51 +0000

    Tiding up day 3

--- src/AdventOfCode2020/Day03.cs
+++ src/AdventOfCode2020/Day03.cs
@@ -1,7 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace AdventOfCode2020
@@ -11,30 +10,24 @@ namespace AdventOfCode2020
         [Fact]
         public void Part_1_example()
         {
-            var lines = Example();
+            var route = Traverse(Example, (3, 1)).ToArray();
 
-            var route = Navigate(lines, (3, 1));
+            Assert.Equal("..#.##.####", new string(route));
 
-            Assert.Equal(".OXOXXOXXXX", route);
-
-            Assert.Equal(7, route.Count(c => c == 'X'));
+            Assert.Equal(7, route.Count(IsTree));
         }
 
         [Fact]
-        public async Task Part_1()
+        public void Part_1()
         {
-            var lines = await Input();
+            var route = Traverse(Input, (3, 1));
 
-            var route = Navigate(lines, (3, 1));
-
-            Assert.Equal(162, route.Count(c => c == 'X'));
+            Assert.Equal(162, route.Count(IsTree));
         }
 
         [Fact]
         public void Part_2_example()
         {
-            var lines = Example();
-
             var slopes = new[]
             {
                 (1, 1),
@@ -44,20 +37,18 @@ namespace AdventOfCode2020
                 (1, 2)
             };
 
-            var routes = slopes.Select(slope => Navigate(lines, slope));
+            var routes = slopes.Select(slope => Traverse(Example, slope));
 
-            var trees = routes.Select(route => route.Count(c => c == 'X'));
+            var trees = routes.Select(route => route.Count(IsTree));
 
             Assert.Equal(new[] { 2, 7, 3, 4, 2 }, trees);
-            
+
             Assert.Equal(336, trees.Aggregate((acc, x) => acc * x));
         }
 
         [Fact]
-        public async Task Part_2()
+        public void Part_2()
         {
-            var lines = await Input();
-
             var slopes = new[]
             {
                 (1, 1),
@@ -67,39 +58,34 @@ namespace AdventOfCode2020
                 (1, 2)
             };
 
-            var routes = slopes.Select(slope => Navigate(lines, slope));
+            var routes = slopes.Select(slope => Traverse(Input, slope));
 
-            var trees = routes.Select(route => route.LongCount(c => c == 'X'));
-            
+            var trees = routes.Select(route => route.LongCount(IsTree));
+
             Assert.Equal(3064612320, trees.Aggregate((acc, x) => acc * x));
         }
 
-        private static string Navigate(string[] map, (int right, int down) vector)
+        private static IEnumerable<char> Traverse(IReadOnlyList<string> map, (int right, int down) vector)
         {
-            var route = new StringBuilder();
-
             var x = 0;
             var y = 0;
 
-            while (y < map.Length)
+            while (y < map.Count)
             {
                 var line = map[y];
 
-                route.Append(line[x % line.Length]);
+                yield return line[x % line.Length];
 
                 x += vector.right;
                 y += vector.down;
             }
-
-            route.Replace('.', 'O', 1, route.Length - 1);
-            route.Replace('#', 'X', 1, route.Length - 1);
-
-            return route.ToString();
         }
 
-        private static string[] Example() => new[]
+        private static bool IsTree(char c) => c == '#';
+
+        private static IReadOnlyList<string> Example { get; } = new[]
         {
-            @"..##.......",
+            "..##.......",
             "#...#...#..",
             ".#....#..#.",
             "..#.#...#.#",
@@ -112,9 +98,6 @@ namespace AdventOfCode2020
             ".#..#...#.#"
         };
 
-        private static Task<string[]> Input()
-        {
-            return File.ReadAllLinesAsync("Day03.input.txt");
-        }
+        private static IReadOnlyList<string> Input { get; } = File.ReadAllLines("Day03.input.txt");
     }
 }
