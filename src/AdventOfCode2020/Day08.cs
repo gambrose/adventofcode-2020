b0742f2f65e5c3f102020ca3103eb3c8f96d72a3
Graham Ambrose <graham.ambrose@gmail.com>
2020-12-08 08:39:13 +0000

    Day 8, part 2

--- src/AdventOfCode2020/Day08.cs
+++ src/AdventOfCode2020/Day08.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 using Xunit;
 
 namespace AdventOfCode2020
@@ -15,10 +13,15 @@ namespace AdventOfCode2020
         public void Part_1() => Assert.Equal(2014, Part1(Input));
 
         [Fact]
-        public void Part_2_example() => Assert.Equal(0, Part2(Example));
+        public void Part_2_example()
+        {
+            Assert.Equal(8, Run(FixedExample));
+
+            Assert.Equal(8, Part2(BrokenExample));
+        }
 
         [Fact]
-        public void Part_2() => Assert.Equal(0, Part2(Input));
+        public void Part_2() => Assert.Equal(2251, Part2(Input));
 
         private static int Part1(ReadOnlyMemory<string> input)
         {
@@ -53,6 +56,68 @@ namespace AdventOfCode2020
 
         private static int Part2(ReadOnlyMemory<string> input)
         {
+            var lines = input.ToArray();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+
+                var (operation, argument) = Parse(input.Span[i]);
+
+                switch (operation)
+                {
+                    case "jmp":
+                        lines[i] = $"nop {argument}";
+                        break;
+                    case "nop":
+                        lines[i] = $"jmp {argument}";
+                        break;
+                }
+
+                if (Run(lines) is { } answer)
+                {
+                    return answer;
+                }
+
+                lines[i] = line;
+            }
+
+            return default;
+        }
+
+        private static int? Run(ReadOnlyMemory<string> input)
+        {
+            int accumulator = 0;
+
+            var visited = new bool[input.Length];
+
+            int i = 0;
+
+            while (visited[i] == false)
+            {
+                visited[i] = true;
+                var (operation, argument) = Parse(input.Span[i]);
+
+                switch (operation)
+                {
+                    case "acc":
+                        accumulator += argument;
+                        i++;
+                        break;
+                    case "jmp":
+                        i += argument;
+                        break;
+                    case "nop":
+                        i++;
+                        break;
+                }
+
+                if (i >= input.Length)
+                {
+                    return accumulator;
+                }
+            }
+
             return default;
         }
 
@@ -71,6 +136,26 @@ jmp -3
 acc -99
 acc +1
 jmp -4
+acc +6".Split(Environment.NewLine);
+
+        private static ReadOnlyMemory<string> BrokenExample { get; } = @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6".Split(Environment.NewLine);
+
+        private static ReadOnlyMemory<string> FixedExample { get; } = @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+nop -4
 acc +6".Split(Environment.NewLine);
 
         private static ReadOnlyMemory<string> Input { get; } = File.ReadAllLines("Day08.input.txt");
