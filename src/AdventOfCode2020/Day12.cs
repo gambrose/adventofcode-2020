9ad5c96911cc26f46b1aeade844cd36222e5e7b7
Graham Ambrose <graham.ambrose@gmail.com>
2020-12-12 07:02:55 +0000

    Day 12, part 2

--- src/AdventOfCode2020/Day12.cs
+++ src/AdventOfCode2020/Day12.cs
@@ -12,13 +12,40 @@ namespace AdventOfCode2020
         public void Part_1_example() => Assert.Equal(25, Part1(Example));
 
         [Fact]
-        public void Part_1() => Assert.Equal(default, Part1(Input));
+        public void Part_1() => Assert.Equal(1533, Part1(Input));
 
         [Fact]
-        public void Part_2_example() => Assert.Equal(default, Part2(Example));
+        public void Part_2_example()
+        {
+            var ship = new Ship2();
+            Assert.Equal((0, 0), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((10, 1), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            ship.Move('F', 10);
+            Assert.Equal((100, 10), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((10, 1), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            ship.Move('N', 3);
+            Assert.Equal((100, 10), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((10, 4), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            ship.Move('F', 7);
+            Assert.Equal((170, 38), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((10, 4), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            ship.Move('R', 90);
+            Assert.Equal((170, 38), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((4, -10), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            ship.Move('F', 11);
+            Assert.Equal((214, -72), (ship.Position.EastWestPosition, ship.Position.NorthSouthPosition));
+            Assert.Equal((4, -10), (ship.Waypoint.EastWestPosition, ship.Waypoint.NorthSouthPosition));
+
+            Assert.Equal(286, Part2(Example));
+        }
 
         [Fact]
-        public void Part_2() => Assert.Equal(default, Part2(Input));
+        public void Part_2() => Assert.Equal(25235, Part2(Input));
 
         private static int Part1(ReadOnlyMemory<string> input)
         {
@@ -36,7 +63,16 @@ namespace AdventOfCode2020
 
         private static long Part2(ReadOnlyMemory<string> input)
         {
-            return default;
+            var ship = new Ship2();
+
+            foreach (var line in input)
+            {
+                var (action, value) = Parse(line);
+
+                ship.Move(action, value);
+            }
+
+            return Math.Abs(ship.Position.EastWestPosition) + Math.Abs(ship.Position.NorthSouthPosition);
         }
 
         private class Ship
@@ -47,34 +83,33 @@ namespace AdventOfCode2020
 
             public int Direction { get; private set; } = 90;
 
-            public void Move(char action, int value)
+            public Ship Move(char action, int value)
             {
                 switch (action)
                 {
                     //Action N means to move north by the given value.
                     case 'N':
                         NorthSouthPosition += value;
-                        return;
+                        return this;
                     //Action S means to move south by the given value.
                     case 'S':
                         NorthSouthPosition -= value;
-                        return;
+                        return this;
                     //Action E means to move east by the given value.
                     case 'E':
                         EastWestPosition += value;
-                        return;
+                        return this;
                     //Action W means to move west by the given value.
                     case 'W':
                         EastWestPosition -= value;
-                        return;
+                        return this;
                     //Action L means to turn left the given number of degrees.
                     case 'L':
-                        Move('R', 360 - value);
-                        return;
+                        return Move('R', 360 - value);
                     //Action R means to turn right the given number of degrees.
                     case 'R':
                         Direction = (Direction + value) % 360;
-                        return;
+                        return this;
                     //Action F means to move forward by the given value in the direction the ship is currently facing.
                     case 'F':
                         var direction = Direction switch
@@ -85,7 +120,84 @@ namespace AdventOfCode2020
                             270 => 'W',
                             _ => throw new NotImplementedException()
                         };
-                        Move(direction, value);
+                        return Move(direction, value);
+                }
+
+                throw new NotImplementedException();
+            }
+        }
+
+        private class Position
+        {
+            public int EastWestPosition { get; set; }
+
+            public int NorthSouthPosition { get; set; }
+
+            public Position Move(char action, int value)
+            {
+                switch (action)
+                {
+                    //Action N means to move north by the given value.
+                    case 'N':
+                        NorthSouthPosition += value;
+                        return this;
+                    //Action S means to move south by the given value.
+                    case 'S':
+                        NorthSouthPosition -= value;
+                        return this;
+                    //Action E means to move east by the given value.
+                    case 'E':
+                        EastWestPosition += value;
+                        return this;
+                    //Action W means to move west by the given value.
+                    case 'W':
+                        EastWestPosition -= value;
+                        return this;
+                }
+
+                throw new NotImplementedException();
+            }
+        }
+
+        private class Ship2
+        {
+            public Position Waypoint { get; } = new Position { EastWestPosition = 10, NorthSouthPosition = 1 };
+
+            public Position Position { get; } = new Position();
+
+            public void Move(char action, int value)
+            {
+                switch (action)
+                {
+                    case 'N':
+                    case 'S':
+                    case 'E':
+                    case 'W':
+                        Waypoint.Move(action, value);
+                        return;
+                    // Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+                    case 'L':
+                        Move('R', 360 - value);
+                        return;
+                    // Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+                    case 'R':
+                        if (value == 90)
+                        {
+                            var (east, north) = (Waypoint.EastWestPosition, Waypoint.NorthSouthPosition);
+                            Waypoint.NorthSouthPosition = -east;
+                            Waypoint.EastWestPosition = north;
+                        }
+                        else if (value > 90)
+                        {
+                            Move('R', 90);
+                            Move('R', value - 90);
+                        }
+
+                        return;
+                    // Action F means to move forward to the waypoint a number of times equal to the given value.
+                    case 'F':
+                        Position.Move('N', Waypoint.NorthSouthPosition * value);
+                        Position.Move('E', Waypoint.EastWestPosition * value);
                         return;
                 }
             }
