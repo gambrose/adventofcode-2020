a865a3d455d09e305ceeb909bf4d51f35b58140f
Graham Ambrose <graham.ambrose@gmail.com>
2020-12-16 05:58:35 +0000

    Day 16, part 2

--- src/AdventOfCode2020/Day16.cs
+++ src/AdventOfCode2020/Day16.cs
@@ -18,11 +18,15 @@ namespace AdventOfCode2020
         [Fact]
         public void Part_2_example()
         {
-            Assert.Equal(default, Part2(Example));
+            var myTicket = ParseMyTicket(Example2);
+
+            Assert.Equal(12, myTicket["class"]);
+            Assert.Equal(11, myTicket["row"]);
+            Assert.Equal(13, myTicket["seat"]);
         }
 
         [Fact]
-        public void Part_2() => Assert.Equal(default, Part2(Input));
+        public void Part_2() => Assert.Equal(3253972369789, Part2(Input));
 
         private static long Part1(ReadOnlyMemory<string> input)
         {
@@ -65,7 +69,70 @@ namespace AdventOfCode2020
 
         private static long Part2(ReadOnlyMemory<string> input)
         {
-            return default;
+            var myTicket = ParseMyTicket(input);
+
+            return myTicket.Where(x => x.Key.StartsWith("departure")).Select(x=> (long)x.Value).Aggregate((acc, x) => acc * x); 
+        }
+
+        private static Dictionary<string, int> ParseMyTicket(ReadOnlyMemory<string> input)
+        {
+            var rules = new List<Rule>();
+            var tickets = new List<int[]>();
+
+            foreach (string line in input)
+            {
+                if (line.Contains('-'))
+                {
+                    rules.Add(Rule.Parse(line));
+                }
+                else if (line.Contains(','))
+                {
+                    tickets.Add(line.Split(',').Select(int.Parse).ToArray());
+                }
+            }
+
+            var myTicket = tickets[0];
+            tickets.RemoveAt(0);
+
+            foreach (var ticket in tickets.ToList())
+            {
+                foreach (var number in ticket)
+                {
+                    if (!rules.Any(rule => rule.Test(number)))
+                    {
+                        tickets.Remove(ticket);
+                        break;
+                    }
+                }
+            }
+
+            var candidates = new List<List<Rule>>();
+
+            for (int i = 0; i < myTicket.Length; i++)
+            {
+                var numbers = tickets.Select(x => x[i]);
+
+                candidates.Add(rules.Where(rule => numbers.All(rule.Test)).ToList());
+            }
+
+            var parsed = new Dictionary<string, int>();
+
+            int index = candidates.FindIndex(x => x.Count == 1);
+
+            while (index >= 0)
+            {
+                var rule = candidates[index][0];
+                parsed.Add(candidates[index][0].Name, myTicket[index]);
+                foreach (var candidate in candidates)
+                {
+                    candidate.Remove(rule);
+                }
+
+                index = candidates.FindIndex(x => x.Count == 1);
+            }
+           
+
+            return parsed;
         }
 
         private class Rule
@@ -138,6 +205,18 @@ nearby tickets:
 40,4,50
 55,2,20
 38,6,12".SplitLines();
+
+        private static ReadOnlyMemory<string> Example2 { get; } = @"class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9".SplitLines();
 
 
         private static ReadOnlyMemory<string> Input { get; } = File.ReadLines("Day16.input.txt").ToArray();
