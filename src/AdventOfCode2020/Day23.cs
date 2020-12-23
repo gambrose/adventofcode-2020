90879a81dfc69f903bb1ff9f908d1a28c22cbf63
Graham Ambrose <graham.ambrose@gmail.com>
2020-12-23 12:06:17 +0000

    Day 23, part 2

--- src/AdventOfCode2020/Day23.cs
+++ src/AdventOfCode2020/Day23.cs
@@ -20,11 +20,10 @@ namespace AdventOfCode2020
         public void Part_1() => Assert.Equal("24798635", Part1(Input));
 
         [Fact]
-        public void Part_2_example() => Assert.Equal(default, Part2(Example));
+        public void Part_2_example() => Assert.Equal(149245887792, Part2(Example));
 
         [Fact]
-        public void Part_2() => Assert.Equal(default, Part2(Input));
-
+        public void Part_2() => Assert.Equal(12757828710, Part2(Input));
 
         private static string Part1(ReadOnlyMemory<string> input, int iterations = 100)
         {
@@ -32,34 +31,39 @@ namespace AdventOfCode2020
 
             var circle = new CupCircle(cups);
 
+            Run(circle, iterations);
+
+            return string.Join("", circle.From(1).Skip(1));
+        }
+
+        private static long Part2(ReadOnlyMemory<string> input)
+        {
+            var cups = Parse(input);
+
+            var circle = new CupCircle(cups, 1000000);
+
+            var foo = circle.From(circle.Start).Take(20).ToList();
+
+            foo = circle.From(1000000 - 4).Take(20).ToList();
+
+            Run(circle, 10000000);
+
+
+            var star1 = circle.Next(1);
+            var star2 = circle.Next(star1);
+
+            return (long)star1 * (long)star2;
+        }
+
+        private static void Run(CupCircle circle, int iterations)
+        {
             var current = circle.Start;
 
-            using var log = new StreamWriter(File.OpenWrite("log.txt"));
+            var pickup = new int[3];
 
             for (int i = 0; i < iterations; i++)
             {
-                log.WriteLine($"-- move {i + 1} --");
-
-                log.Write("cups: ");
-
-                foreach (var cup in circle.From(circle.Start))
-                {
-                    log.Write(cup == current ? '(' : ' ');
-
-                    log.Write(cup);
-                    log.Write(cup == current ? ')' : ' ');
-                }
-                log.WriteLine();
-
-                var pickup = new int[3];
-
-                pickup[0] = circle.Next(current);
-                pickup[1] = circle.Next(pickup[0]);
-                pickup[2] = circle.Next(pickup[1]);
-
-                log.Write("pick up: ");
-                log.Write(string.Join(", ", pickup));
-                log.WriteLine();
+                circle.CopyTo(pickup, circle.Next(current));
 
                 var destination = current;
 
@@ -69,15 +73,10 @@ namespace AdventOfCode2020
 
                     if (destination == 0)
                     {
-                        destination = 9;
+                        destination = circle.Max;
                     }
                 }
                 while (pickup.Contains(destination));
-
-                log.Write($"destination: {destination}");
-                log.WriteLine();
-
-                log.WriteLine();
 
                 circle.Link(current, circle.Next(pickup[2]));
                 circle.Link(pickup[2], circle.Next(destination));
@@ -85,21 +84,15 @@ namespace AdventOfCode2020
 
                 current = circle.Next(current);
             }
-
-            return string.Join("", circle.From(1).Skip(1));
-        }
-
-        private static long Part2(ReadOnlyMemory<string> input)
-        {
-            return default;
         }
 
         class CupCircle
         {
-            private readonly int[] _offsets = new int[9];
+            private readonly int[] _offsets;
 
-            public CupCircle(int[] cups)
+            public CupCircle(int[] cups, int size = 9)
             {
+                _offsets = new int[size];
                 Start = cups[0];
 
                 var previous = Start;
@@ -107,23 +100,41 @@ namespace AdventOfCode2020
                 {
                     var current = cups[i];
 
-                    _offsets[previous - 1] = current - previous;
+                    Link(previous, current);
 
                     previous = current;
                 }
 
-                _offsets[previous - 1] = Start - previous;
+                if (size == 9)
+                {
+                    Link(previous, Start);
+                }
+                else
+                {
+                    Link(previous, cups.Length + 1);
+                    Link(size, Start);
+                }
             }
 
             public int Start { get; }
 
-            public int Count => _offsets.Length;
+            public int Max => _offsets.Length;
 
-            public int Next(int cup) => cup + _offsets[cup - 1];
+            public int Next(int cup) => cup + _offsets[cup - 1] + 1;
 
             public void Link(int left, int right)
             {
-                _offsets[left - 1] = right - left;
+                _offsets[left - 1] = right - left - 1;
+            }
+
+            public void CopyTo(Span<int> destination, int start)
+            {
+                destination[0] = start;
+
+                for (int i = 1; i < destination.Length; i++)
+                {
+                    destination[i] = Next(destination[i - 1]);
+                }
             }
 
             public IEnumerable<int> From(int start)
@@ -132,15 +143,10 @@ namespace AdventOfCode2020
 
                 var cup = start;
 
-                for (int i = 1; i < Count; i++)
+                for (int i = 1; i < Max; i++)
                 {
                     yield return cup = Next(cup);
                 }
-            }
-
-            public override string ToString()
-            {
-                return string.Join(", ", From(Start));
             }
         }
 
